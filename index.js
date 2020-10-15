fe641b6f241916e70daba545a803b7cf292f9338
Philipp Broeker <49947183+pbroeker@users.noreply.github.com>
2020-10-15 09:05:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // saying "Valid. You have a CREDITCARDCOMPANY." where CREDITCARDCOMPANY is either "VISA", "AMEX" or "MASTERCARD".
 
 // You have 45 minutes - 60 minutes (not that I would check on you).
-// Check the test for details. If the test is wrong or doesn't work it is codeworks fault because they didn't teach me correctly ;)
+// Check the test for details.
 
 
 function validate(num) {
